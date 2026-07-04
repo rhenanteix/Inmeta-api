3361e90226e979af586b72c0008337dc53788355
Rhenan Silverio Teixeira <rhenancontato@gmail.com>
2026-07-04 01:38:48 -0300

    create interceptors |00|

---
+++ src/common/interceptors/logging.interceptor.ts
@@ -0,0 +1,19 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor, Logger } from "@nestjs/common";
+import { Observable, tap } from "rxjs";
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+    private readonly logger = new Logger('HTTP')
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        const request = context.switchToHttp().getRequest();
+        const { method, url} = request;
+        const now = Date.now();
+
+
+        return next.handle().pipe(
+            tap(() => {
+                this.logger.log({ method, url },`Request completed in ${Date.now() - now}ms`);
+            })
+        );
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-       bufferLogs:true,
-});
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-    })
-  );
+  const app = await NestFactory.create(AppModule, {bufferLogs:true,});
+  app.useGlobalPipes(new ValidationPipe({whitelist: true,transform: true, forbidNonWhitelisted: true}));
   app.useLogger(app.get(Logger));
-
+  app.useGlobalInterceptors(new LoggingInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT || 3000);
 }
 
